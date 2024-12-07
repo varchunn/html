05072a6208e3a51878603d0e511b545cc1a32303
varchunn <varvaram186@gmail.com>
2024-12-08 00:55:02 +0200

    Update main.cpp1234

--- html/main.cpp
+++ html/main.cpp
@@ -1,14 +1,74 @@
-//
-//  main.cpp
-//  html
-//
-//  Created by Варя Мінєнкова on 08.12.2024.
-//
-
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool validateHTML(const char* filePath) 
+{
+    ifstream file(filePath);
+    if (!file) {
+        cout << "Помилка: файл не знайдено." << endl;
+        return false;
+    }
+    char line[256];
+    char tags[100][50];
+    int tagIndex = 0;
+    while (file.getline(line, 256))
+    {
+        char* pos = strstr(line, "<");
+        while (pos != NULL) {
+            char* end = strstr(pos, ">");
+            if (!end) {
+                cout << "Помилка: Незакритий '<' знайдено." << endl;
+                return false;
+            }
+            char tag[50];
+            strncpy(tag, pos + 1, end - pos - 1);
+            tag[end - pos - 1] = '\0';
+
+            if (tag[0] != '/') 
+            {
+                strcpy(tags[tagIndex++], tag);
+            }
+            else
+            {
+                if (tagIndex == 0 || strcmp(tags[tagIndex - 1], tag + 1) != 0)
+                {
+                    cout << "Помилка: Невідповідний закритий тег </" << tag + 1 << ">." << endl;
+                    return false;
+                }
+                tagIndex--;
+            }
+
+            pos = strstr(end + 1, "<");
+        }
+    }
+
+    if (tagIndex != 0)
+    {
+        cout << "Помилка: Незакритий тег <" << tags[tagIndex - 1] << ">." << endl;
+        return false;
+    }
+
+    file.close();
+    return true;
+}
+
+int main() 
+{
+    char filePath[100];
+    cout << "Введіть шлях до HTML-файлу: ";
+    cin.getline(filePath, 100);
+
+    if (validateHTML(filePath)) 
+    {
+        cout << "HTML файл є валідним." << endl;
+    }
+    else
+    {
+        cout << "HTML файл не є валідним." << endl;
+    }
+    cout << "Натисніть будь-яку клавішу для виходу...";
     return 0;
 }
